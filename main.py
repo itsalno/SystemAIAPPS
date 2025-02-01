b2cb091ac829d954c97f9de9a720d493a84950f3
alekss <anoviconoks@gmail.com>
2025-02-01 15:48:43 +0100

    Added some OOP principles into this Python code.

--- main.py
+++ main.py
@@ -5,69 +5,63 @@ import cpuinfo
 import GPUtil
 
 
+class SystemInfo:
 
+    def __init__(self):
+        self.hostname = socket.gethostname()
+        self.system = platform.system()
+        self.arch = platform.machine()
+        self.kernel = platform.release()
+        self.compiler = platform.python_compiler()
 
-def getSystemInfo():
-    print("———- System Info ———-")
+    def getSystemInfo(self):
+        print("———- System Info ———-")
+        print(f"Hostname : {self.hostname}")
+        print(f"System : {self.system} {self.arch}")
+        print(f"Kernel : {self.kernel}")
+        print(f"Compiler : {self.compiler}")
 
-    hostname = socket.gethostname()
-    print(f"Hostname : {hostname}")
+        cpu = cpuinfo.get_cpu_info()
+        cpu_name = cpu["brand_raw"]
+        cpu_cores = psutil.cpu_count(logical=False)
+        cpu_threads = psutil.cpu_count(logical=True)
+        print(f"CPU : {cpu_name} ({cpu_cores} Cores, {cpu_threads} Threads)")
 
-    system = platform.system()
-    arch = platform.machine()
-    kernel = platform.release()
-    compiler = platform.python_compiler()
+        ram = psutil.virtual_memory().total / (1024 ** 3)
+        print(f"Memory : {ram:.1f} GiB")
 
-    print(f"System : {system} {arch}")
-    print(f"Kernel : {kernel}")
-    print(f"Compiler : {compiler}")
+        disk = psutil.disk_usage('/')
+        disk_total = disk.total / (1024 ** 3)
+        print(f"Disk : {disk_total:.1f} GiB")
 
-    cpu = cpuinfo.get_cpu_info()
-    cpu_name = cpu["brand_raw"]
-    cpu_cores = psutil.cpu_count(logical=False)
-    cpu_threads = psutil.cpu_count(logical=True)
-    print(f"CPU : {cpu_name} ({cpu_cores} Cores, {cpu_threads} Threads)")
+    def getGpuInfo(self):
+        print("\n———- GPU ———-")
+        gpus = GPUtil.getGPUs()
 
-    ram = psutil.virtual_memory().total / (1024 ** 3)
-    print(f"Memory : {ram:.1f} GiB")
+        if not gpus:
+            print("No information! Probably the GPU is onboard!")
+        else:
+            for gpu in gpus:
+                print(f"GPU : {gpu.name} ({gpu.memoryTotal:.1f} GiB VRAM)")
 
+    def getUsageInfo(self):
+        print("\n———- RAM & Disk Usage ———-")
 
-    disk = psutil.disk_usage('/')
-    disk_total = disk.total / (1024 ** 3)
-    print(f"Disk : {disk_total:.1f} GiB")
+        ram = psutil.virtual_memory()
+        ram_used = ram.used / (1024 ** 3)
+        ram_total = ram.total / (1024 ** 3)
+        ram_percent = ram.percent
+        print(f"RAM Used : {ram_used:.1f} GiB / {ram_total:.1f} GiB ({ram_percent}%)")
 
-
-def getGpuInfo():
-    print("\n———- GPU ———-")
-    gpus = GPUtil.getGPUs()
-
-    if not gpus:
-        print("No information! Probably the GPU is onboard!")
-    else:
-        for gpu in gpus:
-            print(f"GPU : {gpu.name} ({gpu.memoryTotal:.1f} GiB VRAM)")
-
-
-def getUsageInfo():
-    print("\n———- RAM & Disk usage ———-")
-
-
-    ram = psutil.virtual_memory()
-    ram_used = ram.used / (1024 ** 3)
-    ram_total = ram.total / (1024 ** 3)
-    ram_percent = ram.percent
-    print(f"RAM Used : {ram_used:.1f} GiB / {ram_total:.1f} GiB ({ram_percent}%)")
-
-
-    disk = psutil.disk_usage('/')
-    disk_used = disk.used / (1024 ** 3)
-    disk_total = disk.total / (1024 ** 3)
-    disk_percent = disk.percent
-    print(f"Disk Used : {disk_used:.1f} GiB / {disk_total:.1f} GiB ({disk_percent}%)")
+        disk = psutil.disk_usage('/')
+        disk_used = disk.used / (1024 ** 3)
+        disk_total = disk.total / (1024 ** 3)
+        disk_percent = disk.percent
+        print(f"Disk Used : {disk_used:.1f} GiB / {disk_total:.1f} GiB ({disk_percent}%)")
 
 
 if __name__ == "__main__":
-    getSystemInfo()
-    getGpuInfo()
-    getUsageInfo()
-
+    system_info = SystemInfo()
+    system_info.getSystemInfo()
+    system_info.getGpuInfo()
+    system_info.getUsageInfo()
